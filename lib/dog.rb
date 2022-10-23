2ada055ef0a406f4745f202f7c7461d0f9a648f8
Tasreen Mowreen <tmowreen97@gmail.com>
2022-10-22 23:45:21 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+require 'pry'
+class Dog
+  #Class body
+
+  #Instance Method definition
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+
+
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Person
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+
+sally = Person.new
+sally.talk
+sally.walk
+
+binding.pry
